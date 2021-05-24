23d3a7642627fcfaf0bffdd6b3bb69c48409ded3
Robert Kieffer <robert@broofa.com>
2021-05-24 14:41:19 -0700

    linting

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 import { useEffect, useState } from 'react';
 
-const TEST_KEY = '__okay';
-
-/**
- * Verify that storage is operational
- */
-function checkStorage(storage) {
-  try {
-    sessionStorage.setItem(TEST_KEY, 'hello');
-    sessionStorage.removeItem(TEST_KEY);
-    return true;
-  } catch (err) {
-    // Intentionally blank
-  }
-
-  return false;
-}
-
-/**
- * Parse a JSON value w/out throwing an error.  Returns is null for invalid JSON
- * strings.
- */
-
 /**
  * React hook for getting/setting state in storage object.  Monitors
  * window.storage events for extrenal changes.
  */
-function useStorage(storage, key, initialState) {
-  function parseStorageValue(json) {
+function useStorage (storage, key, initialState) {
+  /**
+   * Parse a JSON value w/out throwing an error.  Returns is null for invalid JSON
+   * strings.
+   */
+  function parseStorageValue (json) {
     try {
       return JSON.parse(json);
     } catch (err) {
@@ -35,46 +17,46 @@ function useStorage(storage, key, initialState) {
       return null;
     }
   }
-  
+
   const storageValue = parseStorageValue(storage.getItem(key));
   const [value, setValue] = useState(storageValue ?? initialState);
-  
-  function handleStorage({key: k, storageArea, newValue : json}) {
+
+  function handleStorage ({ key: k, storageArea, newValue: json }) {
     if (k === key && storageArea === storage) {
       setValue(parseStorageValue(json));
     }
   }
-  
-  function _setValue(v) {
+
+  function _setValue (v) {
     if (v == null) {
       storage.removeItem(key);
     } else {
       let newValue = v ?? initialState;
       newValue = JSON.stringify(v);
       storage.setItem(key, newValue);
-      
+
       // Propagate in this window since storage events aren't .
-      const e = new StorageEvent('storage');
+      const e = new window.StorageEvent('storage');
       e.initStorageEvent(
-        'storage', 
+        'storage',
         false,
         false,
         key,
         value == null ? null : JSON.stringify(value),
         newValue,
-        String(location),
+        String(window.location),
         storage
       );
 
       window.dispatchEvent(e);
     }
   }
-  
+
   useEffect(() => {
     window.addEventListener('storage', handleStorage);
     return () => window.removeEventListener('storage', handleStorage);
   }, []);
-  
+
   return [value, _setValue];
 }
 
